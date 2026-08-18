e4d5ee3f3571427e467477f33697b3478fb418fb
AlexanderPAI <petrov@backendpages.ru>
2026-08-18 12:55:37 +0300

    Add service add_batch. Refactoring domain tests to service tests

--- service_layer/services.py
+++ service_layer/services.py
@@ -1,5 +1,9 @@
+from datetime import date
+from typing import Optional
+
 from domain import model
 from adapters.repository import AbstractRepository
+from domain.model import OrderLine
 
 
 class InvalidSku(Exception):
@@ -14,9 +18,15 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: model.OrderLine, repo: AbstractRepository, session) -> str:
+def add_batch(ref: str, sku:str, qty: int, eta: Optional[date], repo: AbstractRepository, session):
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
+
+
+def allocate(orderid: str, sku: str, qty: int, repo: AbstractRepository, session) -> str:
+    line = OrderLine(orderid, sku, qty)
     batches = repo.list()
-    if not is_valid_sku(line.sku, batches):
+    if not is_valid_sku(sku, batches):
         raise InvalidSku(f"Недопустимый артикул {line.sku}")
     batchref = model.allocate(line, batches)
     session.commit()
